e631bc1b78ee483e6ec85d9bafc6b58d820b4842
Ibby Hadeed <ibby93@gmail.com>
2017-04-24 17:50:21 -0400

    Update my-module.module.ts

--- src/my-module.module.ts
+++ src/my-module.module.ts
@@ -1,19 +1,16 @@
 import { Observable } from 'rxjs';
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { IonicModule } from 'ionic-angular';
-
 import { MyComponent } from './components/my-component';
 import { MyProvider } from './providers/my-provider';
 
 @NgModule({
   declarations: [
-    MyComponent // declare all components that your module uses
+    // declare all components that your module uses
+    MyComponent
   ],
   exports: [
-    MyComponent // export the component(s) that you want others to be able to use
-  ],
-  imports: [
-    IonicModule // add this if you need to use Ionic components in your module
+    // export the component(s) that you want others to be able to use
+    MyComponent
   ]
 })
 export class MyModule {
